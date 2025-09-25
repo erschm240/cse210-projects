7be5fb1610500b5ea71d80e36c2693603be27eb1
erschm240 <schmitze608@gmail.com>
2025-09-24 18:27:24 -0600

    Completed the C# Prep 4 assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<double> numbers = new List<double>();
+        double number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        while (number != 0)
+        {
+            Console.Write("Chosen number: ");
+            number = double.Parse(Console.ReadLine());
+
+            numbers.Add(number);
+        }
+
+        // Compute the sum of all numbers in the list
+        double totalSum = numbers.Sum();
+        Console.WriteLine($"The sum is: {totalSum}");
+
+        // Compute the average of the list
+        double totalNums = numbers.Count;
+        double average = totalSum / (totalNums - 1);
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the largest number in the list
+        double maxNum = numbers.Max();
+        Console.WriteLine($"The largest number is: {maxNum}");
     }
 }
