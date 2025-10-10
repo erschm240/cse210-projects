0afbcd598f88e845594bbf0a28e330cdcc77623c
erschm240 <schmitze608@gmail.com>
2025-10-09 22:10:22 -0600

    Added comment to explain how I exceeded requirements.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // To exceed requirements, I have stored the time of day and location of user (both user given) as seen in Journal.cs, Entry.cs, and when the journal program is running in the terminal.
         StartMenu startMenu = new StartMenu();
         startMenu.Display();
     }
