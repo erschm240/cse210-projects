e5af84735f0a624066a02c5a6500e2a83843ebb9
erschm240 <schmitze608@gmail.com>
2025-11-24 09:46:35 -0700

    Forgot exceeding requirements comment.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // To exceed requirements I added a display feature that allows the user to view all goals or only the goals that are ongoing, and hide the ones that have been completed. The ongoing-only list also keeps the list number originally assigned to a goal instead of reassigning a list number.
         string userInput = "";
         Menu menu = new Menu();
         do
